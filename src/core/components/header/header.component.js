3a7a07ccc99f44c9e9c30d2016ce9a7df6edb95e
ari7946 <mercado7946@gmail.com>
2022-12-16 13:58:51 -0800

    Add podcast links and styling

--- src/core/components/header/header.component.js
+++ src/core/components/header/header.component.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Nav from '../nav/nav.component';
 import styles from './header.module.scss';
+import PodcastList from '../podcast-list/podcast-list.component';
 
 const Header = ({ handleYear }) => {
   return (
     <header className={styles.header}>
-      <h1 className={styles.headerTitle}>Radio Tu Hora Divina</h1>
+      <div className={styles.topContainer}>
+        <h1 className={styles.headerTitle}>Radio Tu Hora Divina</h1>
+        <PodcastList />
+      </div>
       <Nav handleYear={handleYear} />
     </header>
   )
---
+++ src/core/components/podcast-list/podcast-list.component.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import styles from './podcast-list.module.scss';
+import Image from 'next/image';
+import Link from 'next/link';
+
+
+export default function PodcastList() {
+  return (
+    <div className={styles.wrapper}>
+      <h2 className={styles.subheading}>Escucha Aqui, o en:</h2>
+      <ul className={styles.podcastList}>
+        <li>
+          <div>
+            <Link href="https://podcasts.google.com/feed/aHR0cHM6Ly9hbmNob3IuZm0vcy9kMjBjYzA5Yy9wb2RjYXN0L3Jzcw">
+              {/* eslint-disable-next-line @next/next/no-img-element */}
+              <img src="https://www.gstatic.com/podcasts_console/promote/Spanish_ES/ES_Google_Podcasts_Badge.svg" width="150" height="38" alt="Escúchalo en Google Podcasts" />
+            </Link>
+          </div>
+        </li>
+
+        <li>
+          <div>
+            <Link href="https://podcasts.apple.com/us/podcast/tu-hora-divina/id1659299472">
+              {/* eslint-disable-next-line @next/next/no-img-element */}
+              <img src="/images/apple-podcast.svg" width="150" height="38" alt="Escúchalo en Apple Podcasts"/>
+            </Link>
+          </div>
+        </li>
+
+        <li>
+          <div>
+            <Link href="https://open.spotify.com/show/68i6aFTTVXB9c1afxfsHcx?si=6378bb51ba5d4bf6">
+              {/* eslint-disable-next-line @next/next/no-img-element */}
+              <img src="/images/spotify-podcast.svg" width="150" height="38" alt="Escúchalo en Spotify"/>
+            </Link>
+          </div>
+        </li>
+
+        <li>
+          <div>
+            <Link href="https://soundcloud.com/tu-hora-divina?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing">
+              {/* eslint-disable-next-line @next/next/no-img-element */}
+              <img src="/images/soundcloud-podcast.png" width="100" height="38" alt="Escúchalo en Soundcloud"/>
+            </Link>
+          </div>
+        </li>
+
+        <li>
+          <div>
+            <Link href="https://castbox.fm/channel/id5241199?country=mx">
+              {/* eslint-disable-next-line @next/next/no-img-element */}
+              <img src="/images/castbox-podcast.svg" width="150" height="38" alt="Escúchalo en Castbox"/>
+            </Link>
+          </div>
+        </li>
+      </ul>
+    </div>
+  )
+}
